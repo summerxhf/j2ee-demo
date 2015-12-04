5edc0d29f053d4e28d0da3e7ad731756a8124857
xinghaifang <xinghaifang@dangdang.com>
2015-12-04 09:55:30 +0800

    delete

--- git-test/src/main/java/test.java
+++
@@ -1,6 +0,0 @@
-/**
- * Description:
- * Created  2015/11/20 11:38  by xinghaifang
- */
-public class test {
-}
--- log4j-test/src/main/java/test.java
+++
@@ -1,6 +0,0 @@
-/**
- * Description:
- * Created  2015/12/1 13:53  by xinghaifang
- */
-public class test {
-}
